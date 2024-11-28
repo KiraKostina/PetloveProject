cbeb28ef9f29e3e1607bfcf7d5515efa1a65f68f
KiraKostina <kostina.kira@gmail.com>
2024-11-28 22:31:22 +0200

    add html home , change home & logo

--- src/components/MainComponents/Logo/Logo.jsx
+++ src/components/MainComponents/Logo/Logo.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Logo() {
   return (
     <>
-      <p>PetLove</p>
+      <Link to="/home" style={{ color: "#fff" }}>
+        Petlove
+      </Link>
     </>
   );
 }
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,18 +1,10 @@
-import { useNavigate } from "react-router-dom";
 import Logo from "../../components/MainComponents/Logo/Logo";
 import css from "./MainPage.module.css";
 
 export default function MainPage() {
-  const navigate = useNavigate();
-
-  const handleLogoClick = () => {
-    navigate("/home");
-  };
-
   return (
     <div className={css.main_page}>
-      <div>Hello, pets!</div>
-      <Logo onClick={handleLogoClick} />
+      <Logo />
     </div>
   );
 }
