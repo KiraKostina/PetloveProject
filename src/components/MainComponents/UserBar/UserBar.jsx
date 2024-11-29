cac9883954fe12a6e31ef746dcbdd55ebf2caeba
KiraKostina <kostina.kira@gmail.com>
2024-11-29 22:55:38 +0200

    user

---
+++ src/components/MainComponents/UserBar/UserBar.jsx
@@ -0,0 +1,14 @@
+export default function UserBar() {
+  return (
+    <>
+      {/* className={css.navigator} */}
+      <ul>
+        {/* className={css.container} */}
+        <li>
+          {/* className={getLinkClass} */}
+          <NavLink to="/profile">Name</NavLink>
+        </li>
+      </ul>
+    </>
+  );
+}
