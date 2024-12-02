2121a04924afc0c424d17a0889faa4ba4454f835
KiraKostina <kostina.kira@gmail.com>
2024-12-02 23:29:38 +0200

    upd logo&styles, begin sprite

--- src/components/MainComponents/Logo/Logo.jsx
+++ src/components/MainComponents/Logo/Logo.jsx
@@ -1,9 +1,22 @@
 import { Link } from "react-router-dom";
+import css from "./Logo.module.css";
+import sprite from "../../../assets/sprite.svg";
 
-export default function Logo() {
+export default function Logo({ variant = "main" }) {
+  const logoTextClass = css[`text_${variant}`];
+  const heartClass = css[`heart_${variant}`];
+
   return (
     <>
-      <Link to="/home">Petlove</Link>
+      <Link to="/home">
+        <div className={css.logo}>
+          <p className={logoTextClass}>petl</p>
+          <svg className={heartClass}>
+            <use href={`${sprite}#icon-heart-circle`} />
+          </svg>
+          <p className={logoTextClass}>ve</p>
+        </div>
+      </Link>
     </>
   );
 }
