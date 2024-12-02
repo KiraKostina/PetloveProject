b126d2946115f9ec2f9f2ca399cc9cd71d60e4c1
KiraKostina <kostina.kira@gmail.com>
2024-12-02 23:30:22 +0200

    add header style

--- src/components/MainComponents/Header/Header.jsx
+++ src/components/MainComponents/Header/Header.jsx
@@ -1,13 +1,21 @@
+import { useLocation } from "react-router-dom";
 import AuthNav from "../AuthNav/AuthNav";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
+import css from "./Header.module.css";
 
 export default function Header() {
+  const location = useLocation();
+  const logoVariant = location.pathname === "/home" ? "home" : "other";
+  const isHome = location.pathname === "/home";
+
   return (
-    <header>
-      <Logo />
-      <Nav />
-      <AuthNav />
-    </header>
+    <div className={css.container}>
+      <header className={isHome ? css.headerHome : css.headerOther}>
+        <Logo variant={logoVariant} />
+        <Nav />
+        <AuthNav />
+      </header>
+    </div>
   );
 }
